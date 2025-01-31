46c24e9aaa8f895bd58a713bb80da2323acfb4e3
ChathraNavoda <chathra96@gmail.com>
2025-01-31 12:30:15 +0530

    sqflite offline

--- lib/features/auth/repository/auth_remote_repository.dart
+++ lib/features/auth/repository/auth_remote_repository.dart
@@ -3,10 +3,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:planit/core/constants/constants.dart';
 import 'package:planit/core/services/sp_service.dart';
+import 'package:planit/features/auth/repository/auth_local_repository.dart';
 import 'package:planit/models/user_model.dart';
 
 class AuthRemoteRepository {
   final spService = SpService();
+  final authLocalRepository = AuthLocalRepository();
+
   Future<UserModel> signUp({
     required String name,
     required String email,
@@ -14,22 +17,24 @@ class AuthRemoteRepository {
   }) async {
     try {
       final res = await http.post(
-          Uri.parse(
-            '${Constants.backendUri}/auth/signup',
-          ),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: jsonEncode({
-            'name': name,
-            'email': email,
-            'password': password,
-          }));
+        Uri.parse(
+          '${Constants.backendUri}/auth/signup',
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'name': name,
+          'email': email,
+          'password': password,
+        }),
+      );
 
       if (res.statusCode != 201) {
         throw jsonDecode(res.body)['msg'];
       }
-      return UserModel.fromMap(jsonDecode(res.body));
+
+      return UserModel.fromJson(res.body);
     } catch (e) {
       throw e.toString();
     }
@@ -41,21 +46,23 @@ class AuthRemoteRepository {
   }) async {
     try {
       final res = await http.post(
-          Uri.parse(
-            '${Constants.backendUri}/auth/login',
-          ),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: jsonEncode({
-            'email': email,
-            'password': password,
-          }));
+        Uri.parse(
+          '${Constants.backendUri}/auth/login',
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }),
+      );
 
       if (res.statusCode != 200) {
         throw jsonDecode(res.body)['msg'];
       }
-      return UserModel.fromMap(jsonDecode(res.body));
+
+      return UserModel.fromJson(res.body);
     } catch (e) {
       throw e.toString();
     }
@@ -64,10 +71,10 @@ class AuthRemoteRepository {
   Future<UserModel?> getUserData() async {
     try {
       final token = await spService.getToken();
-      print(token);
       if (token == null) {
         return null;
       }
+
       final res = await http.post(
         Uri.parse(
           '${Constants.backendUri}/auth/tokenIsValid',
@@ -77,8 +84,8 @@ class AuthRemoteRepository {
           'x-auth-token': token,
         },
       );
-      print(res.body);
-      if (res.statusCode != 200) {
+
+      if (res.statusCode != 200 || jsonDecode(res.body) == false) {
         return null;
       }
 
@@ -91,15 +98,15 @@ class AuthRemoteRepository {
           'x-auth-token': token,
         },
       );
-      print(userResponse);
-      if (userResponse.statusCode != 200 || jsonDecode(res.body) == false) {
+
+      if (userResponse.statusCode != 200) {
         throw jsonDecode(userResponse.body)['msg'];
       }
-
       return UserModel.fromJson(userResponse.body);
     } catch (e) {
-      print(e);
-      return null;
+      final user = await authLocalRepository.getUser();
+      print(user);
+      return user;
     }
   }
 }
