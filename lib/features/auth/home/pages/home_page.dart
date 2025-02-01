4d79f666c2932bdd6d5281ad3da6d6e2dc89a18d
ChathraNavoda <chathra96@gmail.com>
2025-02-01 15:50:50 +0530

    task card and font

--- lib/features/auth/home/pages/home_page.dart
+++ lib/features/auth/home/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:planit/core/constants/utils.dart';
+import 'package:planit/features/auth/home/widgets/task_card.dart';
 
 class HomePage extends StatelessWidget {
   static MaterialPageRoute route() =>
@@ -9,8 +12,48 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      appBar: AppBar(
+        title: Text("My Tasks"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(CupertinoIcons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TaskCard(
+                  color: const Color.fromARGB(246, 156, 148, 199),
+                  headerText: 'Hell',
+                  descriptionText:
+                      'This is my first task. This is my first task. This is my first task. This is my first task. This is my first task. This is my first task. This is my first task.',
+                ),
+              ),
+              Container(
+                height: 10,
+                width: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: strengthenColor(
+                      const Color.fromARGB(246, 156, 148, 199), 0.69),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  '10.00AM',
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: "Cera Pro",
         inputDecorationTheme: InputDecorationTheme(
           contentPadding: const EdgeInsets.all(27),
           enabledBorder: OutlineInputBorder(
